35f1cec71ae6e052b8d0bfc6b3dc80a14c3d332b
job12.04LTS <yensung.hafon@gmail.com>
2014-08-25 14:47:03 -0400

    path extra in wsgi.py

--- mainReq/mainReq/wsgi.py
+++ mainReq/mainReq/wsgi.py
@@ -7,7 +7,11 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
+
+sys.path.append('/home/fhafon/mainreq/mainReq')
+sys.path.append('/home/fhafon/mainreq/mainReq/mainReq')
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mainReq.settings")
 
 from django.core.wsgi import get_wsgi_application
